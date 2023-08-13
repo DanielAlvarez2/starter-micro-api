6d4065b97a6bc162151bc5c2de9b24218fa09bf5
DanielAlvarez2 <91741699+DanielAlvarez2@users.noreply.github.com>
2023-08-13 14:40:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let http = require('http');
 let x = 'some random variable';
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write(x);
+    res.write(http);
     res.end();
 }).listen(process.env.PORT || 3000);
