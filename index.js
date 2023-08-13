887a5e95b8e0f1d2ed7bc4a0bd6ec01c2e5f38c1
DanielAlvarez2 <91741699+DanielAlvarez2@users.noreply.github.com>
2023-08-13 14:39:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var http = require('http');
+let http = require('http');
+let x = 'some random variable';
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Sunday Aug 13, 2023');
+    res.write(x);
     res.end();
 }).listen(process.env.PORT || 3000);
