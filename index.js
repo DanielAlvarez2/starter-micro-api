194c43a4566b5eb925bdbea0c97793c6c659006a
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-08-19 11:04:28 -0400

    push from local machine

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let http = require('http');
-let x = 'some random variable';
+let x = 'cloned from github';
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write(x);
