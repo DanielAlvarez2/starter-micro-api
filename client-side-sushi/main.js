f0ab7e72756c4206f4ec63b0c502c02e580bf8bf
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-08-21 12:35:52 -0400

    client-side now connected to Cyclic back-end server

--- client-side-sushi/main.js
+++ client-side-sushi/main.js
@@ -2,7 +2,7 @@ document.querySelector('button').addEventListener('click',getInfo)
 
 async function getInfo(){
     const sushiInput = document.querySelector('input').value
-    const response = await fetch(`http://localhost:8000/api/sushi/${sushiInput}`)
+    const response = await fetch(`http://daniel.cyclic.app/api/sushi/${sushiInput}`)
     const data = await response.json()
     document.querySelector('h2').innerHTML = `${data.japaneseName}<br>${data.englishName}`
     console.log(data);
