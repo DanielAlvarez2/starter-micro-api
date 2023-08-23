53e6143e2dfc9ce3fb809c1ddeddd56aef5718c6
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-08-23 05:58:55 -0400

    FINAL working sushi API with front-end at client-side-sushi.glitch.me

--- client-side-sushi/main.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('button').addEventListener('click',getInfo)
-
-async function getInfo(){
-    const sushiInput = document.querySelector('input').value
-    const response = await fetch(`http://daniel.cyclic.app/api/sushi/${sushiInput}`)
-    const data = await response.json()
-    document.querySelector('h2').innerHTML = `${data.japaneseName}<br>${data.englishName}`
-    console.log(data);
-}
