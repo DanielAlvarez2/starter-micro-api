3097eb2f431453bef79fb1b1afc003b7374e4294
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-08-31 09:38:41 -0400

    revert back to simple node server responding with HTML

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-
-const { MongoClient } = require('mongodb');
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000
-
-const uri = process.env.MONGO_CONNECTION_STRING;
-const client = new MongoClient(uri);
-
-app.get("/items/:my_item", async (req, res) => {
-    let my_item = req.params.my_item;
-    let item = await client.db("my_db")
-                .collection("my_collection")
-                .findOne({my_item: my_item})
-
-    return res.json(item)
+const http = require('http')
+const fs = require('fs')
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, {'Content-Type':'text/html'})
+    fs.readFile('index.html',(error,data)=>{
+        res.write(data)
+        res.end()
+    })
 })
 
-client.connect(err => {
-    if(err){ console.error(err); return false;}
-    // connection to mongo is successful, listen for requests
-    app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
-});
+HTTP_PORT = process.env.PORT || 8080;
+server.listen(HTTP_PORT, ()=>{
+    console.log(`Node Server now listening on port ${HTTP_PORT}`)
+})
