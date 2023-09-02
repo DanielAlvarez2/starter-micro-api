278b75ae49eb50c0af8adff44f442dfee29002e0
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-09-02 11:09:53 -0400

    working nama sushi database

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
-
+const saslprep = require('saslprep');
 dotenv.config();
 const app = express();
-
 const uri = process.env.ATLAS_URI;
-
 const client = new MongoClient(uri);
-
 let PORT = process.env.PORT || 3000;
-
 let connectedClient, db;
-
 async function connectToMongoDB(){
     try {
         connectedClient = await client.connect();
@@ -22,21 +17,16 @@ async function connectToMongoDB(){
     } finally {
         db = connectedClient.db('Nama')
     }
-
 }
-
 app.listen(PORT, () => {
     console.log(`Dan's Server is listening on port ${PORT}`);
 })
-
 connectToMongoDB();
-
 app.get('/nama', async (req, res) =>{
     try {
         let collection = await db.collection('sushi');
-        let users = await collection.find().toArray();
-        res.status(200).json(users);
-
+        let namaSushiList = await collection.find().toArray();
+        res.status(200).json(namaSushiList);
     } catch (error) {
         res.status(500).json({error: "Users could not be returned."})
     }
