90fd0ac5845265b1b5bf63ac263224e31765dba3
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-09-05 06:19:28 -0400

    restore error handling

--- server.js
+++ server.js
@@ -8,17 +8,26 @@ const uri = process.env.ATLAS_URI;
 const client = new MongoClient(uri);
 let PORT = process.env.PORT || 3000;
 let connectedClient, db;
-async function connectToMongoDB(){    
+async function connectToMongoDB(){
+    try {
         connectedClient = await client.connect();
         console.log('Dan connected to MongoDB!');
-        db = connectedClient.db('Nama')    
+    } catch (error) {
+        console.log(error);
+    } finally {
+        db = connectedClient.db('Nama')
+    }
 }
 app.listen(PORT, () => {
     console.log(`Dan's Server is listening on port ${PORT}`);
 })
 connectToMongoDB();
 app.get('/nama', async (req, res) =>{
+    try {
         let collection = await db.collection('sushi');
         let namaSushiList = await collection.find().toArray();
         res.status(200).json(namaSushiList);
+    } catch (error) {
+        res.status(500).json({error: "Users could not be returned."})
+    }
 })
