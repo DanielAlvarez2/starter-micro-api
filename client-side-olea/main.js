110d95ca608c490026a3597c02c31b376500fa91
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-09-07 12:08:21 -0400

    client-side + serverAPI responds w/ 1 large object #32 2:28:50 - 2:56:50

--- client-side-olea/main.js
+++ client-side-olea/main.js
@@ -1,7 +1,7 @@
 document.querySelector('button').addEventListener('click',getSpecial)
 
 async function getSpecial(){
-        const response = await fetch('http://localhost:3000/api/olea');
+        const response = await fetch('https://daniel.cyclic.app/api/olea');
         const data = await response.json();
         console.log(data);            
 }
