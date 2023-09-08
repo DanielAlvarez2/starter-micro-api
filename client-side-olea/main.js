702138109124e9de5a4b987e7ed2b89e46dbf773
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-09-07 23:48:56 -0400

    add client-side/display response in the DOM

--- client-side-olea/main.js
+++ client-side-olea/main.js
@@ -4,5 +4,12 @@ async function getSpecial(){
     let specialItem = document.querySelector('input').value;
     const response = await fetch(`https://upload.cyclic.app/api/olea-items/${specialItem}`);
     const data = await response.json();
-    console.log(data);            
+    console.log(data);        
+    document.querySelector('h2').innerHTML = 
+    `${data.category}<br><br>
+    ${data.name}<br><br>
+    ${data.description}<br><br>
+    ${data.price}<br><br>
+    ${data.allergies}<br>
+    `;    
 }
