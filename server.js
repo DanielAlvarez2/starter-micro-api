708e1327c1e59a2401d3a62e7a5f441876dbe916
Daniel Yllanes <daniel.yllanes@hotmail.com>
2023-11-25 09:47:54 -0500

    change route name to clone

--- server.js
+++ server.js
@@ -223,7 +223,7 @@ app.post('/saveDinnerMenuChanges', (request,response)=>{
 
     setTimeout(()=>response.redirect('/dinner'),250)    
 })
-app.get('/cloneMenus', (request, response) =>{
+app.get('/clone', (request, response) =>{
     db.collection('Specials').find().sort({sequence:1}).toArray()
     .then(data => {
         response.render('cloneMenus.ejs', {info: data})
